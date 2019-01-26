848babbcb9a6058486d24749a8c0979e75bed399
ilya682003 <ilyanfs682003@mail.ru>
2019-01-26 10:54:28 +0300

    switch-caseо

---
+++ switch-case.php
@@ -0,0 +1,21 @@
+<?php
+$chislo = 1;
+
+switch ($chislo)
+{
+    case 5:
+        echo "пять";
+        break;
+    case 4:
+        echo "четыре";
+        break;
+    case 3:
+        echo "три";
+        break;
+    case 2:
+        echo "два";
+        break;
+    case 1:
+        echo "один";
+        break;
+}
