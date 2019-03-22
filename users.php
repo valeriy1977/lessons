5f30f07fa2900b6a74bba5b134bdfb2bdca76df0
valeriy1977 <grigorovich_vv@mail.ru>
2019-03-23 00:18:48 +0300

    Добавил пароль # Conflicts: #	switch-case.php

--- users.php
+++ users.php
@@ -3,13 +3,16 @@
     <input type="text" name="name">
     Фамилия:
     <input type="text" name="surname">
+    Пароль
+    <input type="password" name="password">
     <input type="submit" value="Получить данные">
 </form>
+
 <?php
 require_once "lib/functions.php"; // подключение файла
 
 // сам запрос
-$sql = "SELECT birth FROM users WHERE name='{$_POST['name']}' AND surname='{$_POST['surname']}'";
+$sql = "SELECT birth FROM users WHERE name='{$_POST['name']}' AND surname='{$_POST['surname']}' AND password='{$_POST['password']}'";
 
 // выполняем запрос
 $birth = db($sql);
