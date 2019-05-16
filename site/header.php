a34b77c565138a24767f8186b4fac7d8d13eb1bd
valeriy1977 <grigorovich_vv@mail.ru>
2019-05-16 21:41:45 +0300

    Merge branch 'master' of C:\xampp\htdocs\lessons with conflicts.

--- site/header.php
+++ site/header.php
@@ -2,8 +2,9 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style/style.css">
-    <title>Главная страница</title>
+    <link href="style/style.css" rel="stylesheet" type="text/css" >
+    <title>Document</title>
 </head><?="\n"?>
