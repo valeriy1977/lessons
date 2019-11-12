120f22ae3fa439d04bd856eecbefc5fbb784078e
valeriy1977 <grigorovich_vv@mail.ru>
2019-11-12 16:07:32 +0300

    Сделал блоки

--- site/admin/views/Vpageedit.php
+++ site/admin/views/Vpageedit.php
@@ -4,12 +4,23 @@ $page = $pages->pageForEdit($_GET['id']);
 echo "Редактирование страницы ".$page[0]['menu_name'];
 ?>
 <form method="post">
-    Название страницы в меню <input type="text" name="menu_name" value="<?=$page[0]['menu_name']?>"><br>
-    Отображать в главном меню
-    <select name="visible_in_main_menu">
-        <option value="1">Отображать</option>
-        <option value="0">Скрыть</option>
-    </select>
-    Текст страницы <textarea name="content"><?=$page[0]['content']?></textarea>
-    <input type="submit" value="Сохранить">
+    <div class="form-group">
+        <label for="pageName">Название страницы в меню</label>
+        <input type="text" name="menu_name" value="<?=$page[0]['menu_name']?>"><br>
+    </div>
+
+    <div class="form-group">
+        <label for="visible">Отображать в главном меню</label>
+        <select name="visible_in_main_menu">
+            <option value="1">Отображать</option>
+            <option value="0">Скрыть</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="page_text">Текст страницы </label>
+        <textarea name="content"><?=$page[0]['content']?></textarea><br>
+        <input type="submit" value="Сохранить">
+    </div>
+
 </form>
