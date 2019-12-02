5b75b9af09b442bde85b5d6dae1a5c70975d0252
valeriy1977 <grigorovich_vv@mail.ru>
2019-12-02 20:45:32 +0300

    Заготовка для фото

--- site/body.php
+++ site/body.php
@@ -2,10 +2,10 @@
 <body>
 
 <div id='header' class='row'>
-    <div class='col-md-2'>
-        <h1>НАЗВАНИЕ САЙТА</h1>
+    <div class='col-md-3'>
+        <h3>НАЗВАНИЕ САЙТА</h3>
     </div>
-    <div class='col-md-10 today'>
+    <div class='col-md-9 today'>
         <?php
         $today = new \app\classes\Today();
         $today->getTodayDate();
@@ -14,18 +14,44 @@
       
 </div>
 
-<div id='menu' class='col-md-2'>
+<div id='menu' class='col-md-3'>
     <?php
         require_once "views/Vmenu.php";
      ?>
 </div>
 
-<div id = 'content' class='col-md-9'>
+<div id = 'content' class='col-md-7'>
     <?php
         require_once "views/Vcontent.php";
      ?>
 </div>
 
+<section class="dws-wrapper" >
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="img-block"><img src="img/1.jpg" alt="">
+                    <h3>Блок 1</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="img-block"><img src="img/2.jpg" alt="">
+                    <h3>Блок 2</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="img-block"><img src="img/3.jpg" alt="">
+                    <h3>Блок 3</h3>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="img-block"><img src="img/4.jpg" alt="">
+                    <h3>Блок 4</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 require_once "router.php";
