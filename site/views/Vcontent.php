b62f49a2e65635a06582c247bdcda2738fff48ea
lyubomyr83 <lyubomyr83@mail.ru>
2020-01-14 20:11:40 +0300

    14012020

--- site/views/Vcontent.php
+++ site/views/Vcontent.php
@@ -2,16 +2,33 @@
 $sql = "SELECT content, gallery, gallery_id FROM pages WHERE id='{$_GET['id']}'";
 $content= $db->sql($sql);
 
+$search_gallery = '[gallery-';
+$position = strpos($content[0]['content'], $search_gallery);
 
-echo $content[0]['content'];
-if ($content[0]['gallery'])
+if($position !==FALSE)
 {
-    $sql = "SELECT image FROM gallery_images WHERE gallery_id='{$content[0]['gallery_id']}'";
+    $pos = $position+9;
+    $gallery_number  = $content[0]['content']{$pos};
+  
+    //$gallery_number = substr($content[0], $position);
+    //echo "Галерея номер {$gallery_number}";
+
+    $sql = "SELECT image FROM gallery_images WHERE gallery_id='{$gallery_number}'";
     $images = $db->sql($sql);
-    //var_export($images);
+
+    foreach ($images as $images)
+    {
+        echo "<img class = 'gallery_image' src='img/{$images['image']}'>";
+    }
+
+
 }
 
-foreach ($images as $images)
-{
-    echo "<img class = 'gallery_image' src='img/{$images['image']}'>";
-}
+//echo $content[0]['content'];
+
+
+
+    
+
+
+
