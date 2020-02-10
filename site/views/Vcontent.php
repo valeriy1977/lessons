ccae6a6a16adce7a49a18144c0307a336926b2c9
valeriy1977 <grigorovich_vv@mail.ru>
2020-02-10 21:28:03 +0300

    Поиск и замена

--- site/views/Vcontent.php
+++ site/views/Vcontent.php
@@ -1,4 +1,22 @@
 <?php
+
+// Поиск и замена.
+$text = $content;
+$sql = "SELECT content FROM pages WHERE id='{$_GET['id']}'";
+$content = $db->sql($sql);
+$search_gallery = '[gallery-';
+
+preg_match_all("|\[\[gallery-\.(.*)\]\]|U", $text, $matches);
+if (!empty($matches[1]))
+{
+    foreach ($matches[1] as $row)
+    {
+        $item = $db::getRow("SELECT content FROM pages WHERE '{$_GET['id']}'", $row);
+        $replase = (empty($item[0]['content'])) ? '' : $item[0]['content'];
+        $text = str_ireplace('[[gallery-' . $row . ']]', $replase, $text);
+    }
+}
+//
 $sql = "SELECT content FROM pages WHERE id='{$_GET['id']}'";
 $content= $db->sql($sql);
 
@@ -9,7 +27,7 @@ if($position !==FALSE)
 {
     $pos = $position+9;
     $gallery_number  = $content[0]['content']{$pos};
-  
+
     //$gallery_number = substr($content[0], $position);
     //echo "Галерея номер {$gallery_number}";
 
@@ -24,7 +42,7 @@ if($position !==FALSE)
 
 }
 
-// добавить в массив с контентом картинки с галерей в цикле 
+// добавить в массив с контентом картинки с галерей в цикле
 $position_after_gallery = $pos+2;
 $content[0]['content'] = substr($content[0]['content'], $position_after_gallery);
 echo $content[0]['content'];
